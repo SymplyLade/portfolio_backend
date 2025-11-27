e40398064a9b2f00c46521badbe538ea53a1ca1b
Gbemisola victoria <vgbemisola3@gmail.com>
2025-11-27 16:34:57 +0100

    backend

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+# #!/bin/bash
+# echo "Starting FastAPI..."
+# uvicorn main:app --host 0.0.0.0 --port $PORT
+
+
 #!/bin/bash
 echo "Starting FastAPI..."
-uvicorn main:app --host 0.0.0.0 --port $PORT
+uvicorn main:app --reload --host 127.0.0.1 --port 8000
